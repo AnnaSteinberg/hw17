1dd4824fe41460e6e558617fa7724e9e836f6287
AnnaSteinberg <annasteinb@gmail.com>
2025-09-09 13:35:40 +0300

    Удалены лишние файлы из репозитория

--- build/src/config/config.js
+++
@@ -1,8 +0,0 @@
-// import mysql, {Pool} from 'mysql2/promise'
-import dotenv from 'dotenv';
-import appConf from "../../app-config/app-config.json" with { type: "json" };
-dotenv.config();
-export const configuration = Object.assign(Object.assign({}, appConf), { mongoUri: process.env.MONGO_URI || "dev db address", jwt: {
-        secret: process.env.JWT_SECRET || "super-secret",
-        exp: process.env.JWT_EXP || "1h"
-    } });
