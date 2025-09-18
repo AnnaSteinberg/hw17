b1f526bb5fb236aa35aadd5c30a08a01579f55d0
AnnaSteinberg <annasteinb@gmail.com>
2025-09-18 13:41:21 +0300

    cw_20

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import {errorHandler} from "./errorHandler/errorHandler.js";
 import * as fs from "node:fs";
 import morgan from "morgan";
 import {accountRouter} from "./routes/accountRouter.js";
+import swaggerUi from "swagger-ui-express";
+import swaggerDoc from "../docs/openapi.json"  with {type:"json"};
 
 
 export const launchServer = () => {
@@ -20,6 +22,13 @@ export const launchServer = () => {
         app.use(express.json());
         app.use(morgan('dev'));
         app.use(morgan('combined', {stream:logStream}))
+
+        //============Swagger Docs=========
+        app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc,{
+            swaggerOptions: {
+                supportedSubmitMethods:[]
+            }
+        }));
 
         //==============Routers===================
         app.use('/accounts', accountRouter);
